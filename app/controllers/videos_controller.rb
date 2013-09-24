521689812a2cc3c17086fd84ebce22ac059c3058
Greg Hild <grhild@gmail.com>
2013-09-24 14:16:32 -0500

    Add two more videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,13 @@
 class VideosController < ApplicationController
   def show_gladiator
   end
+
+  def show_air_force_one
+  end
+
+  def show_game_of_thrones
+  end
+
+  def show_modern_family
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Metube::Application.routes.draw do
   get '/videos/gladiator' => 'videos#show_gladiator'
 end
+
+Metube::Application.routes.draw do
+  get '/videos/air_force_one' => 'videos#show_air_force_one'
+end
+
+Metube::Application.routes.draw do
+  get '/videos/game_of_thrones' => 'videos#show_game_of_thrones'
+end
+
+Metube::Application.routes.draw do
+  get '/videos/modern_family' => 'videos#show_modern_family'
+end
